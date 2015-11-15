27a24c3bee0daa6216027ec4a9199e204c7f6a94
kduy <duyvkvn@gmail.com>
2015-11-15 19:46:24 +0100

    done section

--- AutoPartsCrawler/spiders/parts.py
+++ AutoPartsCrawler/spiders/parts.py
@@ -10,40 +10,59 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 import locale
 import urlparse
+import logging
+
 
 from AutoPartsCrawler.items import PartsItem
 
+
+logger = logging.getLogger('PartsCrawlSpider')
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
 class PartsCrawlSpider(CrawlSpider):
+
+
+
     name = 'parts_crawler'
     allowed_domain = ['https://www.parts.com/']
     start_urls = [
         #'https://www.parts.com/index.cfm?fuseaction=store.sectionDiagram&siteid=2&vehicleid=412057&diagram=1381020&section=ELECTRICAL&Title=Audi-Q7-Premium-V6-3.0-%20Liter-GAS'
         #'https://www.parts.com/index.cfm'
-        'https://www.parts.com/index.cfm?fuseaction=store.sectionSearch&storeid=2&vehicleid=412058&section=ENGINE&Title=Audi-Q7-Premium%20Plus-V6-3.0-GAS-OEM-Parts'
+        #'https://www.parts.com/index.cfm?fuseaction=store.sectionSearch&storeid=2&vehicleid=412058&section=ENGINE&Title=Audi-Q7-Premium%20Plus-V6-3.0-GAS-OEM-Parts'
+        'https://www.parts.com/index.cfm?fuseaction=store.MakeSearch&VID=412058&MakeID=2&Make=Audi&ModelYear=2014&MODEL=Q7&Engine=V6-3.0-GAS'
     ]
 
-    def parse(self, response):
 
+    def parse(self, response):
+        allSectionSelector = response.xpath('//div[@class="col-md-4 col-sm-3"]//@href')
+        for url in allSectionSelector.extract():
+            print url
+            yield Request(url, callback=self.parse_section)
+
+
+    def parse_section(self, response):
         allDiagramSelector = response.xpath('//div[@class="col-md-3 col-sm-3"]//div[@class="row"]//@href')
         if not allDiagramSelector:
             diagram_selector = response.xpath('//div[@class="sitem"]/div[@class="onethree-left"]//@href')
             for url in diagram_selector.extract():
                 yield Request(url, callback=self.parse_item)
         else:
-            section = re.match(r'(.*)&section=(\w+)&(.*)', response.url)
-            if not section:
-                section = "unknown"
-            else:
-                section = section.group(2)
+            section =re.match(r'(.*)&section=([\w|%20]+)(&(.*)|$)', response.url).group(2)
             request = Request(allDiagramSelector.extract()[0], callback=self.parse_allDiagram)
             request.meta['section'] = section
             yield request
+        #todo: Group for https://www.parts.com/index.cfm?fuseaction=store.sectionSearch&storeid=2&vehicleid=412058&section=ELECTRICAL&Title=Audi-Q7-Premium%20Plus-V6-3.0-GAS-OEM-Parts
+        #todo: replace %20 by space
 
 
     def parse_allDiagram(self, response):
         diagramsSelector = response.xpath('//div[@class="item-image"]//@href')
         for url in diagramsSelector.extract():
-            print url
             request = Request(url, callback=self.parse_item)
             request.meta['section'] = response.meta['section']
             yield request
@@ -57,12 +76,18 @@ class PartsCrawlSpider(CrawlSpider):
         l.add_xpath('trim', '//*[@class="breadcrumb"]/li[5]/a/strong/text()', MapCompose(unicode.split), Join())
         l.add_xpath('component', '//*[@class="breadcrumb"]/li[6]/text()', MapCompose(unicode.strip))
 
-        section = re.match(r'(.*)&section=(\w+)&(.*)', response.url)
-        if not section:
-            section = response.meta['section']
-        else:
+        section = re.match(r'(.*)&section=([\w|%20]+)(&(.*)|$)', response.url)
+        if  section:
             section = section.group(2)
-        l.add_value('section',section)
+        else:
+            try :
+                section = response.meta['section']
+            except Exception as ex:
+                section = "unknown"
+                logger.debug(response.url)
+
+
+        l.add_value('section',section.replace("%20", " "))
 
         # price
         components = Selector(response).xpath('//fieldset[@class="contentwaiting"]')
@@ -75,6 +100,7 @@ class PartsCrawlSpider(CrawlSpider):
 
         totalPrice = 0.0
         locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+        #todo: some parts are missing
         for key, value in groups.iteritems():
             totalPrice += locale.atof(value)
 
